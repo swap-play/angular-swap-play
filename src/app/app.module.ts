178173100c446953a0e39e7287d6e1320e22c828
João Dias <joaovitorelf@gmail.com>
2023-10-31 19:20:56 -0300

    menu-bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/menu-bar/menu-bar.component.ts
+++ src/app/components/menu-bar/menu-bar.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { faInstagram, faTwitter, faLinkedin, faFacebook, fab } from '@fortawesome/free-brands-svg-icons';
+import { faCircleInfo, faHome } from '@fortawesome/free-solid-svg-icons';
+import { faComment, faBell } from '@fortawesome/free-regular-svg-icons';
 
 @Component({
   selector: 'app-menu-bar',
@@ -6,5 +9,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./menu-bar.component.css']
 })
 export class MenuBarComponent {
+  instagram = faInstagram;
+  twitter = faTwitter;
+  linkedin = faLinkedin;
+  facebook = faFacebook;
+
+  info = faCircleInfo;
+  chat = faComment;
+  notification = faBell;
+  home = faHome;
 
 }
